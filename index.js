fe45e2afe21b89baf8c6d17a63587d2af5cd789f
Logan Sorensen <loganmsorensen@gmail.com>
2020-07-20 16:45:36 -0700

    made contact form functional

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 // Navigation
-const mobileWidth = window.matchMedia("(max-width: 900px)")
-const navBar = document.getElementsByTagName("nav")[0]
-const hamburger = document.querySelector(".fa-bars")
-const navItems = document.querySelectorAll(".nav-item")
-const navItemsCont = document.querySelector(".nav-links")
+const mobileWidth = window.matchMedia("(max-width: 900px)");
+const navBar = document.getElementsByTagName("nav")[0];
+const hamburger = document.querySelector(".fa-bars");
+const navItems = document.querySelectorAll(".nav-item");
+const navItemsCont = document.querySelector(".nav-links");
 
 const widthCheck = (width) => {
   if (width.matches) {
-    hamburger.classList.add("visible")
-    navItemsCont.classList.add("hidden")
+    hamburger.classList.add("visible");
+    navItemsCont.classList.add("hidden");
     navItems.forEach((item) => {
       item.addEventListener("click", () => {
-        navItemsCont.classList.toggle('hidden')
-      })
-    })
+        navItemsCont.classList.toggle("hidden");
+      });
+    });
     hamburger.addEventListener("click", () => {
-      navItemsCont.classList.toggle("hidden")
-    })
+      navItemsCont.classList.toggle("hidden");
+    });
   } else {
-    hamburger.classList.remove("visible")
-    navItemsCont.classList.remove('hidden')
+    hamburger.classList.remove("visible");
+    navItemsCont.classList.remove("hidden");
   }
+};
 
-}
-
-widthCheck(mobileWidth)
-mobileWidth.addListener(widthCheck)
-
-
-
-
+widthCheck(mobileWidth);
+mobileWidth.addListener(widthCheck);
 
 // Project Modal Functions
 const modalTriggers = document.querySelectorAll(".modal-trigger");
@@ -63,9 +58,8 @@ modalTriggers.forEach((trigger) => {
 });
 
 // Skills Accordion
-
 const accordionTriggers = document.querySelectorAll(".accordion-trigger");
-const accordions = document.querySelectorAll(".skill-content")
+const accordions = document.querySelectorAll(".skill-content");
 let open = false;
 
 accordionTriggers.forEach((trigger) => {
@@ -74,9 +68,58 @@ accordionTriggers.forEach((trigger) => {
     const accordion = document.querySelector(
       `[data-accordion="${accordionTrigger}"]`
     );
-    const arrow = document.querySelector(`[data-arrow="${accordionTrigger}"]`)
+    const arrow = document.querySelector(`[data-arrow="${accordionTrigger}"]`);
 
-    arrow.classList.toggle('inverted')
-    accordion.classList.toggle('visible')
+    arrow.classList.toggle("inverted");
+    accordion.classList.toggle("visible");
   });
 });
+
+// Form Submission
+
+window.addEventListener("DOMContentLoaded", function () {
+  var form = document.getElementById("contact-form");
+  var button = document.getElementById("form-button");
+  var status = document.getElementById("form-status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    button.style = "display: none ";
+    status.innerHTML = "Message sent!";
+    status.classList.remove("form-status-error");
+    status.classList.add("form-status-success");
+  }
+
+  function error() {
+    status.innerHTML = "Oops! There was a problem.";
+    status.classList.remove("form-status-success");
+    status.classList.add("form-status-error");
+  }
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
